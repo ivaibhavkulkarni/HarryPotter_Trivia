84ad4f9a5af4584e4a4c097bdc5668558e313793
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-06-21 11:19:15 +0530

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Vaibhav kulkarni on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
