a99a30e0bcabfe709a69fe43de750395b533401b
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-06-26 16:48:31 +0530

    add play button and animation

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -11,6 +11,7 @@ import AVKit
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var animateViewsIn = false
+    @State private var scalePlayButton = false
     
     var body: some View {
         GeometryReader { geo in
@@ -47,6 +48,36 @@ struct ContentView: View {
                     .animation(.easeOut(duration: 0.7).delay(2),value: animateViewsIn)
                     
                     Spacer()
+                    Spacer()
+                    Spacer()
+                    
+                    VStack{
+                        if animateViewsIn {
+                            Button {
+                                //Play a game
+                            }label: {
+                                Text("Play")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(.white)
+                                    .padding(.vertical,7)
+                                    .padding(.horizontal, 50)
+                                    .background(.yellow)
+                                    .clipShape(.rect(cornerRadius: 20))
+                                    .shadow(radius: 5)
+                                    .scaleEffect(scalePlayButton ? 1.2 : 1)
+                                    .onAppear{
+                                        withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+                                            scalePlayButton.toggle()
+                                        }
+                                    }
+                                
+                            }
+                            .transition(.offset(y: geo.size.height/3))
+                        }
+                    }
+                    .animation(.easeOut(duration: 0.7).delay(2),value: animateViewsIn)
+                    
+                    Spacer()
                 }
             }
             .frame(width: geo.size.width, height: geo.size.height)
@@ -54,14 +85,14 @@ struct ContentView: View {
         .ignoresSafeArea()
         .onAppear{
             animateViewsIn = true
-            playAudio()
+          //  playAudio()
         }
     }
     
     private func playAudio() {
         let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
         audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
-        audioPlayer.numberOfLoops = -1
+        audioPlayer.numberOfLoops = 1
         audioPlayer.play()
     }
 }
