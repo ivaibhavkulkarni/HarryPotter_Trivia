3a65a2e82ae64f9cf82241cce96090ff77cc1866
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-06-30 13:27:33 +0530

    recent scores

--- HPTrivia/ViewModels/Game.swift
+++ HPTrivia/ViewModels/Game.swift
@@ -21,6 +21,13 @@ class Game {
                                                         Data(contentsOf: Bundle.main.url(forResource: "trivia", withExtension: "json")!))[0]
     var answers: [String] = []
     
+    let savePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appending(path: "RecentScores")
+    
+    init() {
+        loadScores()
+    }
+    
+    
     func startGame() {
         for book in bookQuestions.books {
             if book .status == .active {
@@ -73,4 +80,23 @@ class Game {
         activeQuestions = []
         answeredQuestions = []
     }
+    
+    func saveScores(){
+        do {
+            let data = try JSONEncoder().encode(recentScores)
+            try data.write(to: savePath)
+        }catch{
+            print("Unable to save data: \(error)")
+        }
+    }
+    
+    func loadScores() {
+        do {
+            let data = try Data(contentsOf: savePath)
+            recentScores = try JSONDecoder().decode([Int].self, from: data)
+        }catch{
+            recentScores = [0,0,0]
+        }
+    }
+    
 }
